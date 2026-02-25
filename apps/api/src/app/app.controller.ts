6cef9f1df9b79c133d94a882e61f6f771ab4fa0c
deepak-kaar <iinkolludeepak@kaartech.com>
2026-02-25 10:56:21 +0300

    add test api

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -5,8 +5,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Get()
+  // getData() {
+  //   return this.appService.getData();
+  // }
+
   @Get()
-  getData() {
-    return this.appService.getData();
-  }
+    getHello() {
+    return 'Hello World';
+    }
 }
